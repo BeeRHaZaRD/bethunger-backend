61f7e932563b11800fd9aaecf4d9a78e7ee19fa4
beerhazard <beerhazard99@gmail.com>
2023-12-05 15:41:28 +0300

    Initial commit

---
+++ src/main/java/com/hg/bethunger/BethungerApplication.java
@@ -0,0 +1,13 @@
+package com.hg.bethunger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BethungerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BethungerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hg/bethunger/BethungerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hg.bethunger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BethungerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
